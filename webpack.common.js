8cf6889705801ac671dfe3bf3f036e58ae3dda78
matthewgreer <matthew.greer.dev@gmail.com>
2020-08-19 13:33:41 -0400

    Fixed order of webpack sass css loaders

--- webpack.common.js
+++ webpack.common.js
@@ -10,19 +10,19 @@ module.exports = {
     publicPath: "/dist/"
   },
   resolve: {
-    extensions: [".js"] // if we were using React.js, we would include ".jsx"
+    extensions: [".js"]
   },
   module: {
     rules: [
       {
-        test: /\.js$/, // if we were using React.js, we would use \.jsx?$/
+        test: /\.js$/,
         use: {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env"],
             plugins: ["@babel/plugin-proposal-optional-chaining"],
             exclude: /node_modules/
-          } // if we were using React.js, we would include "react"
+          } 
         }
       },
       {
@@ -31,8 +31,6 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              // you can specify a publicPath here
-              // by default it uses publicPath in webpackOptions.output
               publicPath: "../",
               hmr: process.env.NODE_ENV === "development"
             }
@@ -47,8 +45,6 @@ module.exports = {
           {
             loader: "file-loader",
             options: {
-              // you can specify a publicPath here
-              // by default it uses publicPath in webpackOptions.output
               name: "[name].[ext]",
               outputPath: "images/",
               publicPath: "images/"
@@ -57,28 +53,25 @@ module.exports = {
         ]
       },
       {
-        test: /\.scss/,
+        test: /\.scss$/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              // you can specify a publicPath here
-              // by default it uses publicPath in webpackOptions.output
               publicPath: "../",
               hmr: process.env.NODE_ENV === "development"
             }
           },
+          // THE ORDER OF THESE 3 IS CRUCIAL!
           "css-loader",
-          "sass-loader",
-          "postcss-loader"
+          "postcss-loader",
+          "sass-loader"
         ]
       }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // all options are optional
       filename: "[name].css",
       chunkFilename: "[id].css",
       ignoreOrder: false // Enable to remove warnings about conflicting order
