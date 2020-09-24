7ecb62404168b33a7e24f1accd2bb6434da17f03
matthewgreer <matthew.greer.dev@gmail.com>
2020-09-24 19:05:27 -0400

    Add comment to Modal

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,4 +1,8 @@
 function Modal() {
+
+  // in future updates, instructions will be factored into click-thru
+  // panels with more visual storytelling.
+
   this.instructions = document.getElementById("modal-shown");
 }
 
@@ -9,6 +13,5 @@ Modal.prototype.closeModal = function closeModal() {
 Modal.prototype.showModal = function showModal(){
   this.instructions.id = "modal-shown";
 };
-
 
 module.exports = Modal;
