c4ac7c0dd5698e04e3545c8a003b263b4adcae91
matthewgreer <matthew.greer.dev@gmail.com>
2020-09-25 23:08:19 -0400

    Replace SCSS variables

--- src/scripts/waves.js
+++ src/scripts/waves.js
@@ -11,14 +11,26 @@ Waves.prototype.draw = function draw(ctx) {
   let sizeScale = this.sizeScale;
 
   const skyCrestToTroughGradient = ctx.createLinearGradient(0, 75, 0, 120);
-  skyCrestToTroughGradient.addColorStop(0, colors.skyCrest);
-  skyCrestToTroughGradient.addColorStop(0.555555556, colors.skyHorizon);
-  skyCrestToTroughGradient.addColorStop(1, colors.skyTrough);
+  skyCrestToTroughGradient.addColorStop(0, "rgba(147, 189, 244, 1.0)");
+  skyCrestToTroughGradient.addColorStop(
+    0.555555556,
+    "rgba(151, 202, 243, 1.0)"
+  );
+  skyCrestToTroughGradient.addColorStop(1, "rgba(154, 210, 243, 1.0)");
+  // skyCrestToTroughGradient.addColorStop(0, colors.skyCrest);
+  // skyCrestToTroughGradient.addColorStop(0.555555556, colors.skyHorizon);
+  // skyCrestToTroughGradient.addColorStop(1, colors.skyTrough);
 
   const oceanCrestToTroughGradient = ctx.createLinearGradient(0, 75, 0, 120);
-  oceanCrestToTroughGradient.addColorStop(0, colors.oceanCrest);
-  oceanCrestToTroughGradient.addColorStop(0.555555556, colors.oceanHorizon);
-  oceanCrestToTroughGradient.addColorStop(1, colors.oceanTrough);
+  oceanCrestToTroughGradient.addColorStop(0, "rgba(90, 255, 255, 1.0)");
+  oceanCrestToTroughGradient.addColorStop(
+    0.555555556,
+    "rgba(87, 247, 250, 1.0)"
+  );
+  oceanCrestToTroughGradient.addColorStop(1, "rgba(85, 242, 247, 1.0)");
+  // oceanCrestToTroughGradient.addColorStop(0, colors.oceanCrest);
+  // oceanCrestToTroughGradient.addColorStop(0.555555556, colors.oceanHorizon);
+  // oceanCrestToTroughGradient.addColorStop(1, colors.oceanTrough);
 
 
   ctx.rect(0, 75, 1200, 45);
