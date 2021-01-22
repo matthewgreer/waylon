2d044535ba057464d365a1019457dac60cf2391f
matthewgreer <matthew.greer.dev@gmail.com>
2021-01-22 10:09:40 -0500

    Add more score styling

--- src/scripts/score.js
+++ src/scripts/score.js
@@ -13,11 +13,18 @@ class Score {
   updateScore = () => {
     if (this.game.progressInFeet < 5280) {
       return this.score.innerHTML = `
-        <div class="score-display">Feet Traveled: <span class="score-numbers">${this.game.progressInFeet}</span></div>
+        <div class="score-display">
+          <div class="score-label">Feet Traveled:</div>
+          
+          <div class="score-numbers">${this.game.progressInFeet}</div>
+        </div>
       `;
     }
     return this.score.innerHTML = `
-      <div class="score-display">Miles Traveled: <span class="score-numbers">${this.calculateMiles()}</span></div>
+      <div class="score-display">
+        <div class="score-label">Miles Traveled: </div>
+        <div class="score-numbers">${this.calculateMiles()}</div>
+      </div>
     `;
   };
 
