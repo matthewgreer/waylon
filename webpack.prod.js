0572ef073ee2ee203ef66be5d4f57908ce7701ad
matthewgreer <matthew.greer.dev@gmail.com>
2024-11-11 13:59:45 -0500

    fix require stmts in webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
