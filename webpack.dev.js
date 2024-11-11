4de0ff87590e8a1e763e39e49932a1ddaa622e75
matthewgreer <matthew.greer.dev@gmail.com>
2024-11-11 12:13:08 -0500

    update webpack config files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,68 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = merge(common, {
-    mode: "development",
-    devtool: "source-map",
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    target: 'web',
     devServer: {
-        contentBase: "./",
-        watchContentBase: true,
-        open: "Google Chrome"
-    }
-});
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        hot: true,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            inject: 'body',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'site.webmanifest', to: 'site.webmanifest' },
+                { from: 'android-chrome-192x192.png', to: 'android-chrome-192x192.png' },
+                { from: 'android-chrome-512x512.png', to: 'android-chrome-512x512.png' },
+                { from: 'apple-touch-icon.png', to: 'apple-touch-icon.png' },
+                { from: 'favicon-16x16.png', to: 'favicon-16x16.png' },
+                { from: 'favicon-32x32.png', to: 'favicon-32x32.png' }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'main.css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]',
+                },
+            },
+        ],
+    },
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,61 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = merge(common, {
-    mode: "production",
-    devtool: "source-map"
-});
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            inject: 'body',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'site.webmanifest', to: 'site.webmanifest' },
+                { from: 'android-chrome-192x192.png', to: 'android-chrome-192x192.png' },
+                { from: 'android-chrome-512x512.png', to: 'android-chrome-512x512.png' },
+                { from: 'apple-touch-icon.png', to: 'apple-touch-icon.png' },
+                { from: 'favicon-16x16.png', to: 'favicon-16x16.png' },
+                { from: 'favicon-32x32.png', to: 'favicon-32x32.png' }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'main.[contenthash].css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]',
+                },
+            }
+        ],
+    },
+};
