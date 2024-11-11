f11695f3aff7e0024a3eea1aee2243f3ede4917a
matthewgreer <matthew.greer.dev@gmail.com>
2024-11-11 17:56:17 -0500

    change publicPath to ./

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = {
         filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-        publicPath: '/',
+        publicPath: './',
     },
     plugins: [
         new CleanWebpackPlugin(),
